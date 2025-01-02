1422843e52400a134cc4d033b0f64f4b9e57af9c
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-01-02 22:21:00 +0800

    Add LigerCCELlama module

--- src/uwul/model/__init__.py
+++ src/uwul/model/__init__.py
@@ -1,4 +1,6 @@
-from transformers import AutoModelForCausalLM, AutoConfig
+from transformers import AutoModelForCausalLM, AutoConfig, LlamaForCausalLM
+from liger_kernel.transformers import apply_liger_kernel_to_llama
+from cut_cross_entropy.transformers import cce_patch
 
 
 class AutoModelForCausalLMFactory:
@@ -8,5 +10,24 @@ class AutoModelForCausalLMFactory:
 
     @classmethod
     def from_config(cls, config_path: str, **kwargs):
+        config = AutoConfig.from_pretrained(config_path, **kwargs)
+        return AutoModelForCausalLM.from_config(config)
+
+
+class LigerCCELlama(LlamaForCausalLM):
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        apply_liger_kernel_to_llama(
+            rope=True, rms_norm=True, fused_linear_cross_entropy=False, swiglu=True
+        )
+        cce_patch("llama")
+        return AutoModelForCausalLM.from_pretrained(*args, **kwargs)
+
+    @classmethod
+    def from_config(cls, config_path: str, **kwargs):
+        apply_liger_kernel_to_llama(
+            rope=True, rms_norm=True, fused_linear_cross_entropy=False, swiglu=True
+        )
+        cce_patch("llama")
         config = AutoConfig.from_pretrained(config_path, **kwargs)
         return AutoModelForCausalLM.from_config(config)
